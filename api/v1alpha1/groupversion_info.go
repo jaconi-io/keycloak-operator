edb2733d3a6c3d4d2a4eb46b4c71c76cffc6cdff
Julian Nodorp <jnodorp@jaconi.io>
2024-10-17 14:58:18 +0200

    feat: re-apply Keycloak API

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,6 +1,6 @@
 // Package v1alpha1 contains API Schema definitions for the keycloak.org v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=keycloak.org.jaconi.io
+// +groupName=keycloak.org
 package v1alpha1
 
 import (
@@ -10,7 +10,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "keycloak.org.jaconi.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "keycloak.org", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
